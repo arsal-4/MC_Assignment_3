83d2619abc6f65a26229f21d0dd2746ecbb92844
Muhammad Arslan <86730477+arsal-4@users.noreply.github.com>
2021-11-09 22:07:11 +0500

    After Createing Learn Activity

--- app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
+++ app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.makhrijal_urfapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.repoLink);
+        btn2 = findViewById(R.id.learn);
+        btn3 = findViewById(R.id.quiz);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openRepoLink();
+            }
+        });
+    }
+
+    public void openRepoLink() {
+        Intent intent = new Intent(this, LearnActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/makhrijal_urfapplication/QuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.makhrijal_urfapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
