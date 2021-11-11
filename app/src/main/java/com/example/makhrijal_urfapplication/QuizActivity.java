2ae9d13672c026640b741356a3e945d0ff36e194
Muhammad Arslan <86730477+arsal-4@users.noreply.github.com>
2021-11-11 21:27:04 +0500

    After creating next button

--- app/src/main/java/com/example/makhrijal_urfapplication/QuizActivity.java
+++ app/src/main/java/com/example/makhrijal_urfapplication/QuizActivity.java
@@ -3,14 +3,70 @@ package com.example.makhrijal_urfapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    int i = 0;
+    TextView txt, txt2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        txt = findViewById(R.id.queNo);
+        txt2 = findViewById(R.id.queSt);
+
+        rg = findViewById(R.id.rg);
+
+        txt.setText(Integer.toString(i+1));
+        txt2.setText(que.get(i));
+
+        for (int index = 0; index < rg.getChildCount(); index++) {
+            ((RadioButton) rg.getChildAt(index)).setText(options.get(index));
+        }
     }
 
+    List<String> que= Arrays.asList("Select the articulation point of the letter ب", "Select the articulation point of the letter د", "Select the articulation point of the letter ع","Select the articulation point of the letter ن", "Select the articulation point of the letter ق");
+    List<String> options= Arrays.asList("LIPS","MOUTH", "NOSE","THROAT", "TONGUE");
+    List<String> selAns= Arrays.asList(".",".", ".",",", ",");
+    List<String> ans= Arrays.asList("LIPS", "TONGUE", "TONGUE", "NOSE", "TONGUE");
 
+    public void nextButton(View view){
+        i++;
+
+        txt = findViewById(R.id.queNo);
+        txt2 = findViewById(R.id.queSt);
+
+        rg = findViewById(R.id.rg);
+        rg.clearCheck();
+
+        txt.setText(Integer.toString(i+1));
+        txt2.setText(que.get(i));
+
+        for (int index = 0; index < rg.getChildCount(); index++) {
+            ((RadioButton) rg.getChildAt(index)).setText(options.get(index));
+        }
+
+        if(i >= 4){
+
+        }
+    }
+
+    public void onClickRadio(View view){
+        rg = findViewById(R.id.rg);
+        int selectedId = rg.getCheckedRadioButtonId();
+        RadioButton btn = findViewById(selectedId);
+        selAns.add(i,btn.getText().toString());
+
+    }
 }
