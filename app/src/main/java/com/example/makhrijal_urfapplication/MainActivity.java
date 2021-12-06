ea522fa384d27b52a3c9f5cb8cef0e1efe37a39d
Muhammad Arslan <86730477+arsal-4@users.noreply.github.com>
2021-12-07 01:39:51 +0500

    After adding a menu to main activity

--- app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
+++ app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
@@ -6,13 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn, btn2, btn3;
+    @Override
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
