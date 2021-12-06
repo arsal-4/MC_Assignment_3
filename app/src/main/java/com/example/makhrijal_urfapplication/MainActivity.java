762f50b975c3eed4a50d1c000cfe9161eb7435f6
Muhammad Arslan <86730477+arsal-4@users.noreply.github.com>
2021-12-07 01:06:00 +0500

    After adding a toolbar

--- app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
+++ app/src/main/java/com/example/makhrijal_urfapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.makhrijal_urfapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         btn = findViewById(R.id.repoLink);
         btn2 = findViewById(R.id.learn);
